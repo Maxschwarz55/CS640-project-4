34d0db73a691886d9a88f08df9df6f59cf811a44
Tyler Richard <trichard2@wisc.edu>
2026-03-26 00:28:17 -0500

    Fix errors

--- edu/wisc/cs/sdn/vnet/rt/Router.java
+++ edu/wisc/cs/sdn/vnet/rt/Router.java
@@ -15,6 +15,7 @@ import net.floodlightcontroller.packet.RIPv2Entry;
 
 import java.util.HashMap;
 import java.util.LinkedList;
+
 /**
  * @author Aaron Gember-Jacobson and Anubhavnidhi Abhashkumar
  */
@@ -22,7 +23,7 @@ public class Router extends Device {
 	/** Routing table for the router */
 	private RouteTable routeTable;
 
-    private HashMap<Integer, RIPTableEntry> ripRouteTable;
+	private HashMap<Integer, RIPTableEntry> ripRouteTable;
 
 	/** ARP cache for the router */
 	private ArpCache arpCache;
@@ -33,8 +34,11 @@ public class Router extends Device {
 	 */
 	public Router(String host, DumpFile logfile, boolean initTable) {
 		super(host, logfile);
-		this.routeTable = initTable? new RouteTable() : null;
-        if (this.routeTable == null) { ripRouteTable = new HashMap(); this.initRIPTable(); }
+		this.routeTable = initTable ? new RouteTable() : null;
+		if (this.routeTable == null) {
+			ripRouteTable = new HashMap<Integer, RIPTableEntry>();
+			this.initRIPTable();
+		}
 		this.arpCache = new ArpCache();
 	}
 
@@ -79,70 +83,64 @@ public class Router extends Device {
 		System.out.println("----------------------------------");
 	}
 
-    public void initRIPTable() {
-        
-        Map<String, Iface> interfaces = this.getInterfaces(); 
-        for (String interfaceName : interfaces.keySet()) {
-            Iface currInterface = interfaces.get(interfaceName);
-            int currInterfaceAddr = currInterface.getIpAddress();
-            int currInterfaceMask = currInterface.getSubnetMask()
-            RIPv2Entry entry = new RIPTableEntry(new RIPv2Entry(currInterfaceAddr, currInterfaceMask, 1), interfaceName);  
-            this.ripRouteTable.put(currInterfaceAddr & currInterfaceMask, entry);
-        }
-    }
+	public void initRIPTable() {
 
-    public void handleRIPPacket(RIPv2 ripPacket, Iface iface) {
-        
-        LinkedList<RIPv2Entry> entries = ripPacket.getEntries();
-        ListIterator<RIPv2Entry> entriesIterator = entries.listIterator();
-        
-        while (entriesIterator.hasNext()) {
-            RIPv2Entry newEntry = entriesIterator.next();
-            int newEntryAddress = newEntry.getAddress();
-            int newEntryMask = newEntry.getSubnetMask();
-            int lookup = newEntryAddress & newEntryMask;
-            if (ripRouteTable.contains(lookup)) {
-                RIPTableEntry currEntry = ripRouteTable.get(lookup);
-                if (newEntry.getMetric() < currEntry.getRIPEntry().getMetric()) {
-                    ripRouteTable.replace(lookup, new RIPTableEntry(newEntry, iface));
-                }
-            }
-            else {
-                ripRouteTable.put(lookup, new RIPTableEntry(newEntry, iface));
-            }
-        }
-    }
+		var interfaces = this.getInterfaces();
+		for (String interfaceName : interfaces.keySet()) {
+			Iface currInterface = interfaces.get(interfaceName);
+			int currInterfaceAddr = currInterface.getIpAddress();
+			int currInterfaceMask = currInterface.getSubnetMask();
+			var entry = new RIPTableEntry(new RIPv2Entry(currInterfaceAddr, currInterfaceMask, 1),
+					currInterface);
+			this.ripRouteTable.put(currInterfaceAddr & currInterfaceMask, entry);
+		}
+	}
 
-    public void sendRIPPacket() {
-        
-        RIPv2 rip = new RIPv2();
-        rip.setCommand(RIPv2.COMMAND_REQUEST);
-        for (RIPTableEntry entry : this.ripRouteTable.values()) {
-            rip.addEntry(entry.getRIPEntry());
-        }
+	public void handleRIPPacket(RIPv2 ripPacket, Iface iface) {
+		for (var newEntry : ripPacket.getEntries()) {
+			int newEntryAddress = newEntry.getAddress();
+			int newEntryMask = newEntry.getSubnetMask();
+			int lookup = newEntryAddress & newEntryMask;
+			if (ripRouteTable.containsKey(lookup)) {
+				RIPTableEntry currEntry = ripRouteTable.get(lookup);
+				if (newEntry.getMetric() < currEntry.getRIPEntry().getMetric()) {
+					ripRouteTable.replace(lookup, new RIPTableEntry(newEntry, iface));
+				}
+			} else {
+				ripRouteTable.put(lookup, new RIPTableEntry(newEntry, iface));
+			}
+		}
+	}
 
-        UDP udp = new UDP();
-        udp.setSourcePort(UDP.RIP_PORT);
-        udp.setDestinationPort(UDP.RIP_PORT);
-        udp.setPayload(rip);
+	public void sendRIPPacket() {
+		RIPv2 rip = new RIPv2();
+		rip.setCommand(RIPv2.COMMAND_REQUEST);
+		for (RIPTableEntry entry : this.ripRouteTable.values()) {
+			rip.addEntry(entry.getRIPEntry());
+		}
 
-        IPv4 ip = new IPv4();
-        ip.setDestinationAddress(0xE0000009);
-        ip.setProtocol(IPv4.PROTOCOL_UDP);
-        ip.setPayload(udp);
+		UDP udp = new UDP();
+		udp.setSourcePort(UDP.RIP_PORT);
+		udp.setDestinationPort(UDP.RIP_PORT);
+		udp.setPayload(rip);
 
-        Ethernet eth = new Ethernet();
-        eth.setEtherType(Ethernet.TYPE_IPv4);
-        eth.setDestinationMACAddress("FF:FF:FF:FF:FF:FF");
-        eth.setPayload(ip);
+		IPv4 ip = new IPv4();
+		ip.setDestinationAddress(0xE0000009);
+		ip.setProtocol(IPv4.PROTOCOL_UDP);
+		ip.setPayload(udp);
 
-        for (RIPTableEntry entry : this.ripRouteTable.values()) {
-            eth.setSourceMACAddress(entry.getIface().getMacAddress().toBytes());
-            ip.setSourceAddress(entry.getIface().getIpAddress());
-            this.sendPacket(eth, entry.getIface());
-        }
+		Ethernet eth = new Ethernet();
+		eth.setEtherType(Ethernet.TYPE_IPv4);
+		eth.setDestinationMACAddress("FF:FF:FF:FF:FF:FF");
+		eth.setPayload(ip);
 
-    }
+		for (RIPTableEntry entry : this.ripRouteTable.values()) {
+			eth.setSourceMACAddress(entry.getIface().getMacAddress().toBytes());
+			ip.setSourceAddress(entry.getIface().getIpAddress());
+			this.sendPacket(eth, entry.getIface());
+		}
+
+	}
 
 	/**
 	 * Handle an Ethernet packet received on a specific interface.
@@ -160,104 +158,103 @@ public class Router extends Device {
 			System.out.println(
 					"*** -> Non IPv4 Packet dropped: " + etherPacket.toString().replace("\n", "\n\t"));
 			return;
+		} else {
+			IPv4 ipPacket = (IPv4) etherPacket.getPayload();
+			if (ipPacket.getProtocol() == IPv4.PROTOCOL_UDP) {
+				UDP udpPacket = (UDP) ipPacket.getPayload();
+				if (udpPacket.getSourcePort() == UDP.RIP_PORT && udpPacket.getDestinationPort() == UDP.RIP_PORT) {
+					RIPv2 ripPacket = (RIPv2) udpPacket.getPayload();
+					handleRIPPacket(ripPacket, inIface);
+				}
+
+			}
+
+			// Verify Checksum
+			var packet = (net.floodlightcontroller.packet.IPv4) etherPacket.getPayload();
+			var checksum = packet.getChecksum();
+			packet.resetChecksum();
+
+			byte[] data = new byte[packet.getHeaderLength() * 4];
+			ByteBuffer bb = ByteBuffer.wrap(data);
+
+			bb.put((byte) (((packet.getVersion() & 0xf) << 4) | (packet.getHeaderLength() & 0xf)));
+			bb.put(packet.getDiffServ());
+			bb.putShort(packet.getTotalLength());
+			bb.putShort(packet.getIdentification());
+			bb.putShort((short) (((packet.getFlags() & 0x7) << 13) | (packet.getFragmentOffset() & 0x1fff)));
+			bb.put(packet.getTtl());
+			bb.put(packet.getProtocol());
+			bb.putShort(packet.getChecksum());
+			bb.putInt(packet.getSourceAddress());
+			bb.putInt(packet.getDestinationAddress());
+			if (packet.getOptions() != null)
+				bb.put(packet.getOptions());
+			bb.rewind();
+			int accumulation = 0;
+			for (int i = 0; i < packet.getHeaderLength() * 2; ++i) {
+				accumulation += 0xffff & bb.getShort();
+			}
+			accumulation = ((accumulation >> 16) & 0xffff)
+					+ (accumulation & 0xffff);
+
+			if (checksum != (short) (~accumulation & 0xffff)) {
+				System.out.println(
+						"*** -> Invalid checksum, packet dropped: " + etherPacket.toString().replace("\n", "\n\t"));
+				return;
+			}
+
+			// Verify TTL
+			var ttl = packet.getTtl();
+			if (ttl <= 1) {
+				System.out.println(
+						"*** -> TTL 0, packet dropped: " + etherPacket.toString().replace("\n", "\n\t"));
+				return;
+			}
+			packet.setTtl((byte) (ttl - 1));
+
+			int destAddr = packet.getDestinationAddress();
+			RouteEntry tableMatch = routeTable.lookup(destAddr);
+			if (tableMatch == null) {
+				System.out.println(
+						"*** -> Destination address not found, packet dropped: "
+								+ etherPacket.toString().replace("\n", "\n\t"));
+				return;
+			}
+
+			if (this.routeTable.lookup(destAddr).getInterface() == inIface) {
+				return;
+			}
+
+			int nextHopAddr = tableMatch.getGatewayAddress();
+			if (nextHopAddr == 0) {
+				nextHopAddr = destAddr;
+			}
+			MACAddress tableMatchMACAddr = arpCache.lookup(nextHopAddr).getMac();
+			etherPacket.setDestinationMACAddress(tableMatchMACAddr.toBytes());
+			Iface outgoingIface = tableMatch.getInterface();
+			etherPacket.setSourceMACAddress(outgoingIface.getMacAddress().toBytes());
+
+			this.sendPacket(etherPacket, outgoingIface);
+
 		}
-        else {
-            IPv4 ipPacket = (IPv4)etherPacket.getPayload();
-            if (ipPacket.getProtocol() == IPv4.PROTOCOL_UDP) {
-                UDP udpPacket = (UDP)ipPacket.getPayload();
-                if (udpPacket.getSourcePort() == UDP.RIP_PORT && udpPacket.getDestinationPort() == UDP.RIP_PORT) {
-                    RIPv2 ripPacket = (RIPv2)udpPacket.getPayload();
-                    handleRIPPacket(ripPacket);
-                }
-
-        }
-
-
-		// Verify Checksum
-		var packet = (net.floodlightcontroller.packet.IPv4) etherPacket.getPayload();
-		var checksum = packet.getChecksum();
-		packet.resetChecksum();
-
-		byte[] data = new byte[packet.getHeaderLength() * 4];
-		ByteBuffer bb = ByteBuffer.wrap(data);
-
-		bb.put((byte) (((packet.getVersion() & 0xf) << 4) | (packet.getHeaderLength() & 0xf)));
-		bb.put(packet.getDiffServ());
-		bb.putShort(packet.getTotalLength());
-		bb.putShort(packet.getIdentification());
-		bb.putShort((short) (((packet.getFlags() & 0x7) << 13) | (packet.getFragmentOffset() & 0x1fff)));
-		bb.put(packet.getTtl());
-		bb.put(packet.getProtocol());
-		bb.putShort(packet.getChecksum());
-		bb.putInt(packet.getSourceAddress());
-		bb.putInt(packet.getDestinationAddress());
-		if (packet.getOptions() != null)
-			bb.put(packet.getOptions());
-        bb.rewind();
-		int accumulation = 0;
-		for (int i = 0; i < packet.getHeaderLength() * 2; ++i) {
-			accumulation += 0xffff & bb.getShort();
-		}
-		accumulation = ((accumulation >> 16) & 0xffff)
-				+ (accumulation & 0xffff);
-
-		if (checksum != (short) (~accumulation & 0xffff)) {
-			System.out.println(
-					"*** -> Invalid checksum, packet dropped: " + etherPacket.toString().replace("\n", "\n\t"));
-			return;
-		}
-
-		// Verify TTL
-		var ttl = packet.getTtl();
-		if (ttl <= 1) {
-			System.out.println(
-					"*** -> TTL 0, packet dropped: " + etherPacket.toString().replace("\n", "\n\t"));
-			return;
-		}
-		packet.setTtl((byte) (ttl - 1));
-
-
-        int destAddr = packet.getDestinationAddress();     
-        RouteEntry tableMatch = routeTable.lookup(destAddr);
-        if (tableMatch == null) {
-            System.out.println(
-                    "*** -> Destination address not found, packet dropped: " + etherPacket.toString().replace("\n", "\n\t"));
-            return;
-        }
-
-        if (this.routeTable.lookup(destAddr).getInterface() == inIface)
-        { return; }
-        
-        int nextHopAddr = tableMatch.getGatewayAddress();
-        if (nextHopAddr == 0) {
-            nextHopAddr = destAddr;
-        }
-        MACAddress tableMatchMACAddr = arpCache.lookup(nextHopAddr).getMac();
-        if (tableMatch == null) {
-            System.out.println("*** -> No route found, dropping");
-            return;
-        }
-        etherPacket.setDestinationMACAddress(tableMatchMACAddr.toBytes());
-        Iface outgoingIface = tableMatch.getInterface();
-        etherPacket.setSourceMACAddress(outgoingIface.getMacAddress().toBytes());
-        
-        this.sendPacket(etherPacket, outgoingIface);
-
 	}
+
+	private class RIPTableEntry {
+		public RIPv2Entry ripEntry;
+		public Iface iface;
+
+		public RIPTableEntry(RIPv2Entry ripEntry, Iface iface) {
+			this.ripEntry = ripEntry;
+			this.iface = iface;
+		}
+
+		public RIPv2Entry getRIPEntry() {
+			return this.ripEntry;
+		}
+
+		public Iface getIface() {
+			return this.iface;
+		}
+	}
+
 }
-
-private class RIPTableEntry {
-    public RIPv2Entry ripEntry;
-    public Iface iface;
-
-    public RIPTableEntry(RIPv2Entry ripEntry, Iface iface) {
-        this.ripEntry = ripEntry;
-        this.iface = iface;
-    }
-
-    public RIPv2Entry getRIPEntry() { return this.ripEntry; }
-
-    public Iface getIface() { return this.iface; }
-}
-
-
