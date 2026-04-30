ed704e848c4ec0cb89df0a1ee47f0c7cc10f2269
Tyler Richard <trichard2@wisc.edu>
2026-04-30 14:18:30 -0500

    Make router more reliable

--- src/edu/wisc/cs/sdn/vnet/rt/Router.java
+++ src/edu/wisc/cs/sdn/vnet/rt/Router.java
@@ -173,7 +173,7 @@ public class Router extends Device {
 
 		double rand = Math.random();
 		// dropping a pakcet with 5% probablity
-		if (rand < 0.30) {
+		if (rand < 0.05) {
 			System.out.println("Randomly dropping a packet");
 			return;
 		}
